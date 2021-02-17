0dadb54addf3cc09cde789ec7e73f5ab23025939
Craig Ryan <craig@craigryan.org>
2021-02-17 15:00:40 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template
+
+
+app = Flask(__name__)  # creates instance of class, stores imported classes ^
+
+
+@app.route("/")  # all functions are objects so can be passed around @
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
