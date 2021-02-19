a4a3348fceef34295ddafdb0b33d8b56039ed693
Craig Ryan <craig@craigryan.org>
2021-02-19 11:48:49 +0000

    Added styling to recipes cards

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 
@@ -7,22 +8,26 @@ app = Flask(__name__)  # creates instance of class, stores imported classes ^
 
 @app.route("/")  # all functions are objects so can be passed around @
 def index():
-    return render_template("index.html")
+    return render_template("index.html", )
 
 
 @app.route("/recipes")
 def recipes():
-    return render_template("recipes.html")
+  data = []
+  with open("data/recipes.json", "r") as json_data:
+    data = json.load(json_data)
+    return render_template(
+      "recipes.html", page_title="All Recipes", recipes=data)
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
-@app.route("/blank")
-def blank():
-    return render_template("blank.html")
+@app.route("/recommend")
+def recommend():
+    return render_template("recommend.html", page_title="Our Recommendations")
 
 
 if __name__ == "__main__":
